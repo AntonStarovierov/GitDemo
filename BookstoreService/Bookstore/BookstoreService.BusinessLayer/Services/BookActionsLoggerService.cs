c15076e837fa97ef0ea9ddda2c78de72dcbf184e
STONE046\anton.starovierov <anton.starovierov@28stone.com>
2018-01-16 16:57:30 +0200

    Added bookstore services

---
+++ BookstoreService/Bookstore/BookstoreService.BusinessLayer/Services/BookActionsLoggerService.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using BookstoreService.Base.Dtos;
+using BookstoreService.Base.ServiceCommunicator;
+using Newtonsoft.Json;
+
+namespace BookstoreService.BusinessLayer.Services
+{
+	public class BookActionsLoggerService : IBookActionsLoggerService
+	{
+		private readonly IServiceCommunicationFactory factory;
+
+		public BookActionsLoggerService(IServiceCommunicationFactory factory)
+		{
+			this.factory = factory;
+		}
+
+		public async Task LogBookAction(string token, LogParameters parameters, string url)
+		{
+			using (var communicator = factory.GetServiceCommunicator(token))
+			{
+				await communicator.PutAsync(url, JsonConvert.SerializeObject(parameters));
+			}
+		}
+	}
+
+	public interface IBookActionsLoggerService
+	{
+		Task LogBookAction(string token, LogParameters parameters, string url);
+	}
+}
---
+++ BookstoreService/Bookstore/BookstoreService.BusinessLayer/Services/BookService.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BookstoreService.DataAccessLayer.Entities;
+using BookstoreService.DataAccessLayer.Repositories;
+
+namespace BookstoreService.BusinessLayer.Services
+{
+	public interface IBookService
+	{
+		Task<IEnumerable<Book>> GetAsync();
+		Task<Book> GetById(int id);
+		Task<int> AddAsync(Book book);
+		Task DeleteAsync(int id);
+		Task UpdateAsync(int id, Book newBook);
+	}
+
+	public class BookService : IBookService
+	{
+		private readonly IBookRepository _repository;
+
+		public BookService(IBookRepository repository)
+		{
+			this._repository = repository;
+		}
+
+		public async Task<IEnumerable<Book>> GetAsync()
+		{
+			return await _repository.GetAsync();
+		}
+
+		public async Task<Book> GetById(int id)
+		{
+			return await _repository.GetById(id);
+		}
+
+		public async Task<int> AddAsync(Book book)
+		{
+			return await _repository.AddAsync(book);
+		}
+
+		public async Task DeleteAsync(int id)
+		{
+			await _repository.DeleteAsync(id);
+		}
+
+		public async Task UpdateAsync(int id, Book newBook)
+		{
+			await _repository.UpdateAsync(id, newBook);
+		}
+	}
+}
