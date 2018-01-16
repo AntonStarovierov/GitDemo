71d8d5f0a592f25b00140a24b9241e1a29b21de7
STONE046\anton.starovierov <anton.starovierov@28stone.com>
2018-01-16 17:02:43 +0200

    Adjusted bookstore host

--- BookstoreService/Bookstore/BookstoreService.Host/Startup.cs
+++ BookstoreService/Bookstore/BookstoreService.Host/Startup.cs
@@ -1,40 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BookstoreService.Base.Auth;
+using BookstoreService.Base.Configuration;
+using BookstoreService.Base.ServiceCommunicator;
+using BookstoreService.BusinessLayer.Services;
+using BookstoreService.DataAccessLayer.Contexts;
+using BookstoreService.DataAccessLayer.Contexts.DbInit;
+using BookstoreService.DataAccessLayer.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace BookstoreService.Host
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json", false, true)
+				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+				.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<BookDbContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("BookstoreConnection")));
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			services.AddTransient<IBookRepository, BookRepository>();
+			services.AddTransient<IBookService, BookService>();
 
-            app.UseMvc();
-        }
-    }
+			services.AddTransient<IServiceCommunicator, ServiceCommunicator>();
+			services.AddTransient<IServiceCommunicationFactory, ServiceCommunicationFactory>();
+
+			services.AddTransient<IBookActionsLoggerService, BookActionsLoggerService>();
+
+			services.Configure<ServiceConfiguration>(Configuration.GetSection("ServicesUrls"));
+
+			services.AddMvc();
+		}
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, BookDbContext context)
+		{
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddDebug();
+			StartupConfigureAuth.ConfigureAuth(app);
+			app.UseMvc();
+			BookDbInitializer.Initialize(context);
+		}
+	}
 }
