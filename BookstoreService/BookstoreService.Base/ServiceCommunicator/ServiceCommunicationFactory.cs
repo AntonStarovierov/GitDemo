a213fd55b83781572d038dedb3257593b4df085f
STONE046\anton.starovierov <anton.starovierov@28stone.com>
2018-01-16 16:14:12 +0200

    Added service communicator

---
+++ BookstoreService/BookstoreService.Base/ServiceCommunicator/ServiceCommunicationFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+
+namespace BookstoreService.Base.ServiceCommunicator
+{
+	public class ServiceCommunicationFactory : IServiceCommunicationFactory
+	{
+		public IServiceCommunicator GetServiceCommunicator(string token)
+		{
+			var client = CreateHttpClient(token);
+			var communicator = new ServiceCommunicator(client);
+			return communicator;
+		}
+
+		private static HttpClient CreateHttpClient(string token = "")
+		{
+			var client = new HttpClient();
+			if (!string.IsNullOrWhiteSpace(token))
+			{
+				client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+			}
+
+			return client;
+		}
+	}
+
+	public interface IServiceCommunicationFactory
+	{
+		IServiceCommunicator GetServiceCommunicator(string token);
+	}
+}
---
+++ BookstoreService/BookstoreService.Base/ServiceCommunicator/ServiceCommunicator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreService.Base.ServiceCommunicator
+{
+	public class ServiceCommunicator : IServiceCommunicator
+	{
+		private readonly HttpClient _httpClient;
+
+		public ServiceCommunicator(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		public async Task<string> GetAsync(string url)
+		{
+			var response = await _httpClient.GetAsync(url);
+			return await response.Content.ReadAsStringAsync();
+		}
+
+		public async Task<string> PutAsync(string url, string objectJson)
+		{
+			var content = new StringContent(objectJson, Encoding.UTF8, "application/json");
+			var response = await _httpClient.PutAsync(url, content);
+			return await response.Content.ReadAsStringAsync();
+		}
+
+		public async Task<string> PostAsync(string url, string objectJson)
+		{
+			var content = new StringContent(objectJson, Encoding.UTF8, "application/json");
+			var response = await _httpClient.PostAsync(url, content);
+			return await response.Content.ReadAsStringAsync();
+		}
+
+		public async Task<string> DeleteAsync(string url)
+		{
+			var response = await _httpClient.DeleteAsync(url);
+			return await response.Content.ReadAsStringAsync();
+		}
+
+		public void Dispose()
+		{
+			_httpClient?.Dispose();
+		}
+	}
+
+	public interface IServiceCommunicator : IDisposable
+	{
+		Task<string> GetAsync(string url);
+		Task<string> PutAsync(string url, string objectJson);
+		Task<string> PostAsync(string url, string objectJson);
+		Task<string> DeleteAsync(string url);
+	}
+}
