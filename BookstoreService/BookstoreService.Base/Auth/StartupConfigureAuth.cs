65506843bfda4026e18a0ca57da9658759f29fdf
STONE046\anton.starovierov <anton.starovierov@28stone.com>
2018-01-17 16:06:43 +0200

    Commit with tag

--- BookstoreService/BookstoreService.Base/Auth/StartupConfigureAuth.cs
+++ BookstoreService/BookstoreService.Base/Auth/StartupConfigureAuth.cs
@@ -9,6 +9,7 @@ namespace BookstoreService.Base.Auth
 		{
 			var a = 1;
 			var b = 2;
+			var c = 3;
 			app.UseJwtBearerAuthentication(new JwtBearerOptions
 			{
 				AutomaticAuthenticate = true,
